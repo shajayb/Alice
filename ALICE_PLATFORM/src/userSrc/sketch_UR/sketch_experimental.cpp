0989329e3483844a6f1a6332dfdc0df69fc80b10
shajay <shajay.bhooshan@gmail.com>
2016-09-28 14:28:04 +0100

    Add files via upload

---
+++ ALICE_PLATFORM/src/userSrc/sketch_UR/sketch_experimental.cpp
@@ -0,0 +1,72 @@
+#include "main.h"
+#include "ALICE_ROBOT_DLL.h"
+using namespace ROBOTICS;
+#include <array>
+#include <memory>
+//#include<iostream>
+//#include <experimental\coroutine>
+#include<experimental/generator> 
+using namespace std;
+using namespace std::experimental;
+
+/*
+NOTE: all experimental features require that you add "/await" to the command line options in  ( properties -> c++ -> commandline options)
+*/
+
+
+
+auto strip(const char *p)
+{
+	while (*p)
+		yield *p++;
+}
+
+
+generator<int> anonymousList()
+{
+	for (int i = 0; i < 10; i++)yield i;
+}
+
+
+
+
+
+void setup()
+{
+
+	for (auto c : strip("myName")) cout << c << ".";
+	cout << endl;
+	
+	for (auto c : anonymousList() ) cout << c << ".";
+	cout << endl;
+	
+}
+	
+void update(int value)
+{
+
+	
+}
+
+void draw()
+{
+
+	backGround(0.75);
+
+}
+void keyPress(unsigned char k, int xm, int ym)
+{
+
+		
+}
+void mousePress(int b, int state, int x, int y)
+{
+
+}
+void mouseMotion(int x, int y)
+{
+	
+}
+
+
+
